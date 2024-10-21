6cfaddd818aaada76edbe81d2a5cbb0050134cdc
Saruf Ratul <43202031+Saruf-Ratul@users.noreply.github.com>
2024-10-21 10:52:48 +0600

    ok

--- Project.Domain/obj/Debug/net8.0/Project.Domain.AssemblyInfo.cs
+++ Project.Domain/obj/Debug/net8.0/Project.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c341f744a2f5fb3b4f53d82db9f69077437c1801")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("hello")
+// This function logs "hello" to the console
+function sayHello() {
+    console.log("hello");
+}
+
+// Call the function
+sayHello();
